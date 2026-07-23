a5bb4ffc2f87f9bb099007089da5d7d8e9feaa00
a672548102-crypto <a672548102@gmail.com>
2026-07-23 22:25:45 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,62 @@
+const cases = [
 
+{
+id:"01",
+
+title:"爆款短视频案例01",
+
+type:"剧情短视频",
+
+role:"策划 + 剪辑 + 运营",
+
+views:"280万",
+
+likes:"12万",
+
+cover:"images/cover01.jpg",
+
+link:"case01.html"
+
+},
+
+
+{
+id:"02",
+
+title:"爆款短视频案例02",
+
+type:"品牌宣传",
+
+role:"策划 + 剪辑 + 运营",
+
+views:"150万",
+
+likes:"8万",
+
+cover:"images/cover02.jpg",
+
+link:"case02.html"
+
+},
+
+
+{
+id:"03",
+
+title:"爆款短视频案例03",
+
+type:"探店视频",
+
+role:"策划 + 剪辑 + 运营",
+
+views:"90万",
+
+likes:"5万",
+
+cover:"images/cover03.jpg",
+
+link:"case03.html"
+
+}
+
+];
